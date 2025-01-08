ad8b5f364bcc926837b8907fdf640de51f74a96b
Matias <matiassanchez13422@gmail.com>
2025-01-08 00:34:11 -0300

    Se creo el footer y se agrego el codigo para poder bajarlo

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,23 @@
-import { useState } from 'react'
+import { useState } from "react";
 import { BrowserRouter, Routes, Route } from "react-router";
-import './App.css'
+import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import Menu from'./components/pages/common/Menu'
-function App() {
- 
+import Menu from "./components/pages/common/Menu";
+import Footer from "./components/pages/common/Footer";
 
+
+function App() {
   return (
     <>
-    <BrowserRouter>
-    <Menu></Menu>
-   
-    </BrowserRouter> 
+      <BrowserRouter>
+        <Menu></Menu>
+        <Routes>
+          <Route></Route>
+        </Routes>
+        <Footer></Footer>
+      </BrowserRouter>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/pages/common/Footer.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import '../css/Style.css'
+
+const Footer = () => {
+    return (
+        <div>
+            <section>
+            <div className="app-container">
+      <main className="content">
+      </main>
+      <footer className="footer">
+        <p>© Todos los derechos reservados</p>
+      </footer>
+    </div>
+        </section>
+        </div>
+    );
+};
+
+export default Footer;
