d515d8a4d1fd79744cf44e342692e2dd8ce9bcc0
Matias <matiassanchez13422@gmail.com>
2025-01-08 15:04:02 -0300

    Se creo la funcion para crear un prodcuto API

---
+++ src/components/helpers/queries.js
@@ -0,0 +1,16 @@
+export const crearProductoAPI = async (juegoNuevo) => {
+  try {
+    const respuesta = await fetch("http://localhost:3000/productos", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(juegoNuevo),
+    });
+    console.log(respuesta);
+    return respuesta;
+  } catch (error) {
+    console.error(error);
+    return false;
+  }
+};
