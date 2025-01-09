b772c61a0b2f73d0c0eb1b12fd2f84d67e863525
Matias <matiassanchez13422@gmail.com>
2025-01-08 23:20:01 -0300

    Se agreagaron las redirecciones de administrador

--- src/components/pages/common/Menu.jsx
+++ src/components/pages/common/Menu.jsx
@@ -3,21 +3,21 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import "bootstrap/dist/css/bootstrap.min.css";
 import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
+import { NavLink} from "react-router";
 
 const Menu = () => {
   return (
     <div>
       <Navbar expand="lg" className="bg-body-tertiary">
         <Container>
-          <Navbar.Brand href="#home">GoodGamer</Navbar.Brand>
+          <NavLink  className="nav-link" to={"/"}>GoodGamer</NavLink>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             {/* Añadido ms-auto para mover los enlaces al final */}
             <Nav className="ms-auto">
-              <Nav.Link href="#home">Inicio</Nav.Link>
-              <Nav.Link href="#home">Juegos</Nav.Link>
-              <Nav.Link href="#link">Administrador</Nav.Link>
+              <NavLink  className="nav-link" to={"/"}>Inicio</NavLink>
+              <NavLink  className="nav-link" to={"/Juegos"}>Juegos</NavLink>
+              <NavLink  className="nav-link" to={"/Administrador"}>Administrador</NavLink>
             </Nav>
           </Navbar.Collapse>
         </Container>
