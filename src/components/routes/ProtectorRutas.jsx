fe4364ed3dbd6f79a3a5d21d791134c8f3adec7a
Matias <matiassanchez13422@gmail.com>
2025-01-10 00:51:58 -0300

    Se termino de proteger las rutas para que las personas que no son Administradores no puedan iniciar en Administrador

--- src/components/routes/ProtectorRutas.jsx
+++ src/components/routes/ProtectorRutas.jsx
@@ -1,11 +1,15 @@
-import React from 'react';
+import React from "react";
+import { Navigate } from "react-router-dom"; 
 
-const ProtectorRutas = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+const ProtectorRutas = ({children}) => {
+  const userAdmin = JSON.parse(sessionStorage.getItem("userKey")) || "";
+
+  if (!userAdmin) {
+    return <Navigate to="/Login" />; 
+  } else {
+    return children; 
+  }
 };
 
 export default ProtectorRutas;
+
--- src/components/routes/RutasAdministrador.jsx
+++ src/components/routes/RutasAdministrador.jsx
@@ -1,11 +1,19 @@
-import React from 'react';
+import React from "react";
+import { Route, Routes } from "react-router-dom"; 
+import Administrador from "../pages/Administrador";
+import FormularioJuego from "../pages/FormularioJuego";
 
 const RutasAdministrador = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<Administrador />} />
+        <Route path="/FormularioJuego" element={<FormularioJuego crearProducto={true} />} />
+        <Route path="/editar/:id" element={<FormularioJuego crearProducto={false} />} />
+      </Routes>
+    </div>
+  );
 };
 
 export default RutasAdministrador;
+
