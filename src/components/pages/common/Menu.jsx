aedbe7ed6ccb324319ae7be7ae86bc6318e91e39
Matias <matiassanchez13422@gmail.com>
2025-01-10 00:22:37 -0300

    Se agrego el logout cuando se hace el login y se elimina el Administrador si no estas logueado

--- src/components/pages/common/Menu.jsx
+++ src/components/pages/common/Menu.jsx
@@ -1,25 +1,62 @@
 import React from "react";
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
 import "bootstrap/dist/css/bootstrap.min.css";
-import Navbar from 'react-bootstrap/Navbar';
-import { NavLink} from "react-router";
+import Navbar from "react-bootstrap/Navbar";
+import { NavLink, useNavigate } from "react-router";
+import { Button } from "react-bootstrap";
 
-const Menu = () => {
+
+const Menu = ({ usuarioLogeado, setusuarioLogeado }) => {
+  const navegacion = useNavigate();
+
+
+  const logout = () => {
+    sessionStorage.removeItem("userKey");
+    setusuarioLogeado("");
+    navegacion("/");
+  };
+
   return (
     <div>
       <Navbar expand="lg" className="bg-body-tertiary">
         <Container>
-          <NavLink  className="nav-link" to={"/"}>GoodGamer</NavLink>
+          <NavLink className="nav-link" to={"/"}>
+            GoodGamer
+          </NavLink>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             {/* Añadido ms-auto para mover los enlaces al final */}
             <Nav className="ms-auto">
-              <NavLink  className="nav-link" to={"/"}>Inicio</NavLink>
-              <NavLink  className="nav-link" to={"/Juegos"}>Juegos</NavLink>
-              <NavLink  className="nav-link" to={"/AcercadeNostros"}>Acerca de Nosotros</NavLink>
-              <NavLink  className="nav-link" to={"/Administrador"}>Administrador</NavLink>
-              <NavLink  className="nav-link" to={"/Login"}>Login</NavLink>
+              <NavLink className="nav-link" to={"/"}>
+                Inicio
+              </NavLink>
+              <NavLink className="nav-link" to={"/Juegos"}>
+                Juegos
+              </NavLink>
+              <NavLink className="nav-link" to={"/AcercadeNostros"}>
+                Acerca de Nosotros
+              </NavLink>
+              {usuarioLogeado && usuarioLogeado.length > 0 ? (
+                <>
+                  <NavLink className="nav-link" to={"/Administrador"}>
+                    Administrador
+                  </NavLink>
+                  <Nav.Item>
+                    <Button 
+                      className="nav-link btn btn-link" 
+                      variant="link"
+                      onClick={logout}
+                    >
+                      Logout
+                    </Button>
+                  </Nav.Item>
+                </>
+              ) : (
+                <NavLink className="nav-link" to={"/Login"}>
+                  Login
+                </NavLink>
+              )}
             </Nav>
           </Navbar.Collapse>
         </Container>
---
+++ src/components/routes/ProtectorRutas.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ProtectorRutas = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default ProtectorRutas;
